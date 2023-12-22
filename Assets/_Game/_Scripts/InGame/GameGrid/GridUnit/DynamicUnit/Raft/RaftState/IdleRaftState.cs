ab19aca25716d0f13929b0f843975c2e284ab5b7
HeldenChill <71561065+HeldenChill@users.noreply.github.com>
2023-12-22 18:49:43 +0700

    ~FIX: Raft and Chump Idle Anim

--- Assets/_Game/_Scripts/InGame/GameGrid/GridUnit/DynamicUnit/Raft/RaftState/IdleRaftState.cs
+++ Assets/_Game/_Scripts/InGame/GameGrid/GridUnit/DynamicUnit/Raft/RaftState/IdleRaftState.cs
@@ -1,12 +1,34 @@
 ﻿using _Game.DesignPattern.StateMachine;
+using DG.Tweening;
+using Unity.Mathematics;
+using UnityEngine;
 
 namespace _Game.GameGrid.Unit.DynamicUnit.Raft.RaftState
 {
     public class IdleRaftState : IState<Raft>
     {
+        private float MOVE_Y_VALUE = 0.1f;
+        private float MOVE_Y_TIME = 2f;
+
+        Vector3 originTransform;
         public void OnEnter(Raft t)
         {
+            #region ANIM
+            //DEV: Refactor
+            originTransform = t.Tf.transform.position;
+            DOVirtual.Float(0, MOVE_Y_TIME, MOVE_Y_TIME, i =>
+            {
+                SetSinWavePosition(i);
+            }).SetLoops(-1).SetEase(Ease.Linear);
 
+
+
+            void SetSinWavePosition(float time)
+            {
+                float value = Mathf.Sin(2 * time * Mathf.PI / MOVE_Y_TIME) * MOVE_Y_VALUE;
+                t.Tf.transform.position = originTransform + Vector3.up * value;
+            }
+            #endregion
         }
 
         public void OnExecute(Raft t)
