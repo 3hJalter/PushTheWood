24ee739e284927a5736aa57b00a19974cc6e79e8
HeldenChill <71561065+HeldenChill@users.noreply.github.com>
2023-12-22 10:48:34 +0700

    ~FIX: Change Logic from Roll and Turn over to Fall

--- Assets/_Game/_Scripts/InGame/GameGrid/GridUnit/DynamicUnit/Chump/ChumpState/FallChumpState.cs
+++ Assets/_Game/_Scripts/InGame/GameGrid/GridUnit/DynamicUnit/Chump/ChumpState/FallChumpState.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using _Game.DesignPattern.StateMachine;
 using DG.Tweening;
+using GameGridEnum;
 using UnityEngine;
 
 namespace _Game.GameGrid.Unit.DynamicUnit.Chump.ChumpState
@@ -16,13 +17,28 @@ namespace _Game.GameGrid.Unit.DynamicUnit.Chump.ChumpState
                 return;
             }
 
-            OnExecute(t);
-        }
+            //NOTE: Fall into water that do not have anything
+            if (t.TurnOverData.enterMainCell.SurfaceType is GridSurfaceType.Water
+                & t.TurnOverData.enterMainCell.GetGridUnitAtHeight(Constants.DirFirstHeightOfSurface[GridSurfaceType.Water]) == t)
+            {
+                // Tween to final position
+                t.Tf.DOMove(t.EnterPosData.finalPos, Constants.MOVING_TIME).SetEase(Ease.Linear)
+                    .SetUpdate(UpdateType.Fixed).OnComplete(() =>
+                    {
+                        t.OnEnterTrigger(t);
+                        // Can be change to animation later
+                        t.skin.localRotation =
+                            Quaternion.Euler(t.UnitTypeXZ is UnitTypeXZ.Horizontal
+                                ? Constants.HorizontalSkinRotation
+                                : Constants.VerticalSkinRotation);
+                        // minus position offsetY
+                        t.Tf.position -= Vector3.up * t.yOffsetOnDown;
+                        t.ChangeState(StateEnum.Idle);
+                    });
+            }
 
-        public void OnExecute(Chump t)
-        {
             GameGridCell mainCell = t.MainCell;
-            List<GameGridCell> cellInUnits = new(t.cellInUnits);
+            List<GameGridCell> cellInUnits = new(t.cellInUnits); //DEV: Why new List here?
             t.SetEnterCellData(Direction.None, t.MainCell, t.UnitTypeY, false, t.cellInUnits);
             t.OnOutCells();
             t.OnEnterCells(mainCell, cellInUnits);
@@ -32,6 +48,11 @@ namespace _Game.GameGrid.Unit.DynamicUnit.Chump.ChumpState
                     t.OnEnterTrigger(t);
                     t.ChangeState(StateEnum.Idle);
                 });
+        }
+
+        public void OnExecute(Chump t)
+        {
+            
         }
 
         public void OnExit(Chump t)
