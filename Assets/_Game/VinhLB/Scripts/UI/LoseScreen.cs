518ce065120f7cd2358d27463a5b23b46cc6ba50
ManInVx20 <vinhlb.2k@gmail.com>
2024-01-25 16:44:12 +0700

    ~FIX: Fix bugs in reward popup; Adjust some UI screens +ADD: Trample shader for mesh

--- Assets/_Game/VinhLB/Scripts/UI/LoseScreen.cs
+++ Assets/_Game/VinhLB/Scripts/UI/LoseScreen.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using _Game.GameGrid;
 using _Game.Managers;
 using _Game.DesignPattern;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace VinhLB
 {
     public class LoseScreen : UICanvas
     {
+        [SerializeField]
+        private CanvasGroup _canvasGroup;
+        [SerializeField]
+        private Image _blockPanel;
+
+        public override void Open()
+        {
+            base.Open();
+            
+            DOVirtual.Float(0, 1, 0.25f, value => _canvasGroup.alpha = value)
+                .OnComplete(() => _blockPanel.gameObject.SetActive(false));
+        }
+
         public void OnClickRestartButton()
         {
             LevelManager.Ins.OnRestart();
--- Assets/_Game/_Scripts/UIs/Screen/WinScreen.cs
+++ Assets/_Game/_Scripts/UIs/Screen/WinScreen.cs
@@ -1,9 +1,26 @@
 ﻿using _Game.GameGrid;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace _Game.UIs.Screen
 {
     public class WinScreen : UICanvas
     {
+        [SerializeField]
+        private CanvasGroup _canvasGroup;
+        [SerializeField]
+        private Image _blockPanel;
+
+        public override void Open()
+        {
+            base.Open();
+            Debug.Log(_canvasGroup.alpha);
+            
+            DOVirtual.Float(0, 1, 0.25f, value => _canvasGroup.alpha = value)
+                .OnComplete(() => _blockPanel.gameObject.SetActive(false));
+        }
+        
         public void OnClickNextButton()
         {
             LevelManager.Ins.OnNextLevel();
