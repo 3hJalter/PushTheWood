c29ca85afdb0317919bc9c610299da43c864cb53
HeldenChill <71561065+HeldenChill@users.noreply.github.com>
2024-01-30 10:57:41 +0700

    .

---
+++ Assets/_Game/_Scripts/InGame/GameGrid/GridUnit/DynamicUnit/Player/PlayerState/SitDownPlayerState.cs
@@ -0,0 +1,28 @@
+using _Game.DesignPattern.StateMachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace _Game.GameGrid.Unit.DynamicUnit.Player.PlayerState
+{
+    public class SitDownPlayerState : IState<Player>
+    {
+        public StateEnum Id => throw new System.NotImplementedException();
+
+        public void OnEnter(Player t)
+        {
+            
+        }
+
+        public void OnExecute(Player t)
+        {
+            
+        }
+
+        public void OnExit(Player t)
+        {
+            
+        }
+    }
+}
