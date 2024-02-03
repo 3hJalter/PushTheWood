22b0b2c8c0684de3a5d1b4e57bb3e416d34bbb46
Hà Huy Hoàng <101927338+3hJalter@users.noreply.github.com>
2024-02-03 17:52:01 +0700

    #Fix: Chump Foam

--- Assets/_Game/_Scripts/InGame/GameGrid/GridUnit/DynamicUnit/Chump/ChumpState/IdleChumpState.cs
+++ Assets/_Game/_Scripts/InGame/GameGrid/GridUnit/DynamicUnit/Chump/ChumpState/IdleChumpState.cs
@@ -1,32 +1,36 @@
 ﻿using _Game.DesignPattern.StateMachine;
-using _Game.Utilities;
-using _Game.Utilities.Timer;
 using DG.Tweening;
 using GameGridEnum;
 using UnityEngine;
+
 namespace _Game.GameGrid.Unit.DynamicUnit.Chump.ChumpState
 {
     public class IdleChumpState : IState<Chump>
     {
         private const float MOVE_Y_VALUE = 0.06f;
         private const float MOVE_Y_TIME = 2f;
-
+        private const float FLOATING_Y_OFFSET = 0.1f;
         private Tween floatingTween;
-        
-        Vector3 originTransform;
+
+        private Vector3 originTransform;
 
         public StateEnum Id => StateEnum.Idle;
 
         public void OnEnter(Chump t)
         {
+
             #region ANIM
+
             //DEV: Refactor anim system
             if (t.IsInWater())
             {
-                originTransform = new Vector3(t.Tf.position.x, 
-                    (float)(Constants.DirFirstHeightOfSurface[GridSurfaceType.Water] + t.FloatingHeightOffset) / 2 * Constants.CELL_SIZE - t.yOffsetOnDown, 
-                    t.Tf.position.z);
-                floatingTween = DOVirtual.Float(0 ,MOVE_Y_TIME, MOVE_Y_TIME, SetSinWavePosition).SetLoops(-1).SetEase(Ease.Linear);
+                Vector3 position = t.Tf.position;
+                originTransform = new Vector3(position.x,
+                    (float)(Constants.DirFirstHeightOfSurface[GridSurfaceType.Water] + t.FloatingHeightOffset) / 2 *
+                    Constants.CELL_SIZE - t.yOffsetOnDown + FLOATING_Y_OFFSET,
+                    position.z);
+                floatingTween = DOVirtual.Float(0, MOVE_Y_TIME, MOVE_Y_TIME, SetSinWavePosition).SetLoops(-1)
+                    .SetEase(Ease.Linear);
             }
             else
             {
@@ -35,13 +39,15 @@ namespace _Game.GameGrid.Unit.DynamicUnit.Chump.ChumpState
             }
 
             return;
-            
+
             void SetSinWavePosition(float time)
             {
                 float value = Mathf.Sin(2 * time * Mathf.PI / MOVE_Y_TIME) * MOVE_Y_VALUE;
                 t.Tf.transform.position = originTransform + Vector3.up * value;
             }
+
             #endregion
+
         }
 
         public void OnExecute(Chump t)
