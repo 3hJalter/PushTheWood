c8ef715d50ad37d9a69b73ed78918d8b7c5dd83f
HeldenChill <71561065+HeldenChill@users.noreply.github.com>
2024-02-22 13:42:49 +0700

    +ADD: ButtonCD

---
+++ Assets/_Game/_Scripts/UIs/Utilities/ButtonCD.cs
@@ -0,0 +1,43 @@
+using _Game.Utilities.Timer;
+using Newtonsoft.Json.Bson;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Game.Utilities
+{
+    public class ButtonCD : MonoBehaviour
+    {
+        [SerializeField]
+        float cooldownTime;
+        [SerializeField]
+        Button button;
+        Coroutine coroutine;
+        private void Awake()
+        {
+            button.onClick.AddListener(WaitCooldown);
+        }
+
+        IEnumerator ActiveCooldownTime()
+        {
+            yield return new WaitForSeconds(cooldownTime);
+            button.enabled = true;
+        }
+
+        public void WaitCooldown()
+        {
+            button.enabled = false;
+            coroutine = StartCoroutine(ActiveCooldownTime());
+        }
+
+        public void SetActive(bool active)
+        {
+            if(active) 
+            {
+                StopCoroutine(coroutine);
+                button.enabled = true;
+            }
+        }
+    }
+}
