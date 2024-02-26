d8f2af6b765738c4e30e76e647e59cb41f8ee34a
Vinh Lê Bá <vinhlb.2k@gmail.com>
2024-02-26 17:07:58 +0700

    ~FIX: Add tutorial popup UI

---
+++ Assets/_Game/VinhLB/Scripts/ScriptableObjects/TutorialUIData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace VinhLB
+{
+    [CreateAssetMenu(fileName = "TutorialUIData", menuName = "ScriptableObjects/TutorialUIData", order = 4)]
+    public class TutorialUIData : SerializedScriptableObject
+    {
+        public Dictionary<TutorialType, TutorialUI[]> TutorialUIDict = new();
+    }
+
+    [System.Serializable]
+    public class TutorialUI
+    {
+        public Sprite TutorialSprite;
+        public string Description;
+    }
+
+    public enum TutorialType
+    {
+        None = -1,
+        Test = 0,
+    }
+}
---
+++ Assets/_Game/VinhLB/Scripts/UI/Popups/TutorialPopup.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace VinhLB
+{
+    public class TutorialPopup : UICanvas
+    {
+        [SerializeField]
+        private TutorialUIData _tutorialUIData;
+
+        [SerializeField]
+        private Image _tutorialImage;
+        [SerializeField]
+        private TMP_Text _descriptionText;
+        [SerializeField]
+        private Button _leftButton;
+        [SerializeField]
+        private Button _rightButton;
+
+        private TutorialUI[] _tutorialUIs;
+        private int _currentTutorialUIIndex;
+
+        private void Awake()
+        {
+            _leftButton.onClick.AddListener(() =>
+            {
+                _currentTutorialUIIndex = Mathf.Clamp(_currentTutorialUIIndex - 1, 0, _tutorialUIs.Length);
+                
+                UpdateTutorialUI();
+            });
+            _rightButton.onClick.AddListener(() =>
+            {
+                _currentTutorialUIIndex = Mathf.Clamp(_currentTutorialUIIndex + 1, 0, _tutorialUIs.Length);
+                
+                UpdateTutorialUI();
+            });
+        }
+
+        public override void Setup(object param = null)
+        {
+            base.Setup(param);
+
+            if (param is TutorialType tutorialType)
+            {
+                SetupTutorialUI(tutorialType);
+            }
+            else
+            {
+                SetupTutorialUI((TutorialType)0);
+            }
+        }
+
+        private void SetupTutorialUI(TutorialType tutorialType)
+        {
+            _tutorialUIs = _tutorialUIData.TutorialUIDict[tutorialType];
+
+            _currentTutorialUIIndex = 0;
+            
+            UpdateTutorialUI();
+        }
+
+        private void UpdateTutorialUI()
+        {
+            _tutorialImage.sprite = _tutorialUIs[_currentTutorialUIIndex].TutorialSprite;
+            _descriptionText.text = _tutorialUIs[_currentTutorialUIIndex].Description;
+
+            if (_currentTutorialUIIndex == 0)
+            {
+                _leftButton.gameObject.SetActive(false);
+            }
+            else if (_currentTutorialUIIndex == _tutorialUIs.Length - 1)
+            {
+                _rightButton.gameObject.SetActive(false);
+            }
+            else
+            {
+                _leftButton.gameObject.SetActive(true);
+                _rightButton.gameObject.SetActive(true);
+            }
+        }
+    }
+}
