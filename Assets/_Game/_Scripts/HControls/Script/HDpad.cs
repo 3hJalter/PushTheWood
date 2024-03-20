d6b8fe019ebfbf23cac3c307e96241664eca6418
Hà Huy Hoàng <101927338+3hJalter@users.noreply.github.com>
2024-03-20 09:13:34 +0700

    #MODIFY: Increase Video Size

--- Assets/_Game/_Scripts/HControls/Script/HDpad.cs
+++ Assets/_Game/_Scripts/HControls/Script/HDpad.cs
@@ -7,18 +7,8 @@ namespace HControls
         [Tooltip("0 is Left, 1 is Right, 2 is Up, 3 is Down")]
         [SerializeField]
         private bool highlightButton = true;
-        [SerializeField]
-        private bool testing;
 
         [SerializeField] private HDpadButton[] dpadButtons;
-
-        #if UNITY_EDITOR
-        private void Awake()
-        {
-            testing = true;
-        }
-        #endif
-
 
         private void OnDisable()
         {
@@ -38,31 +28,5 @@ namespace HControls
             HInputManager.SetDirectionInput(Direction.None);
         }
 
-        private void Update()
-        {
-            if (testing)
-            {
-                if (Input.GetKey(KeyCode.A))
-                {
-                    HInputManager.SetDirectionInput(Direction.Left);
-                }
-                else if (Input.GetKey(KeyCode.W))
-                {
-                    HInputManager.SetDirectionInput(Direction.Forward);
-                }
-                else if (Input.GetKey(KeyCode.D))
-                {
-                    HInputManager.SetDirectionInput(Direction.Right);
-                }
-                else if (Input.GetKey(KeyCode.S))
-                {
-                    HInputManager.SetDirectionInput(Direction.Back);
-                }
-                else
-                {
-                    HInputManager.SetDirectionInput(Direction.None);
-                }
-            }
-        }
     }
 }
