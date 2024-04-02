50cc1a9c8f717f66ce2868a551d1678384634492
Hà Huy Hoàng <101927338+3hJalter@users.noreply.github.com>
2024-04-02 17:03:56 +0700

    #FIX: DPAD

--- Assets/_Game/_Scripts/HControls/Script/HDpad.cs
+++ Assets/_Game/_Scripts/HControls/Script/HDpad.cs
@@ -1,3 +1,6 @@
+using System;
+using _Game.Camera;
+using _Game.Managers;
 using UnityEngine;
 
 namespace HControls
@@ -8,7 +11,30 @@ namespace HControls
         [SerializeField]
         private bool highlightButton = true;
 
+        [SerializeField] private HHoldingButton holdingButton;
+        
+        
         [SerializeField] private HDpadButton[] dpadButtons;
+
+
+        private void Awake()
+        {
+            holdingButton.OnInit(Constants.HOLD_TOUCH_TIME, null, PointerUpAction, HoldAction);
+        }
+        
+        void PointerUpAction()
+        {
+            if (CameraManager.Ins.IsCurrentCameraIs(ECameraType.ZoomOutCamera)) CameraManager.Ins.ChangeCamera(ECameraType.InGameCamera, Constants.ZOOM_OUT_TIME);
+            HInputManager.LockInput(false);
+        }
+
+        // Set up holding button
+        void HoldAction()
+        {
+            if (!holdingButton.IsHolding) return;
+            HInputManager.LockInput();
+            CameraManager.Ins.ChangeCamera(ECameraType.ZoomOutCamera, Constants.ZOOM_OUT_TIME);
+        }
 
         private void OnDisable()
         {
