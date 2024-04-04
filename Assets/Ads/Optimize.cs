91ad760a1f866801e582c1dff98b3ada59a8b73b
HeldenChill <71561065+HeldenChill@users.noreply.github.com>
2024-04-04 16:10:39 +0700

    +ADD: Script

---
+++ Assets/Ads/Optimize.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Optimize
+{
+    [ContextMenu("Combine Meshes")]
+    private static Mesh CombineMeshes(List<MeshFilter> sourceMeshFilters)
+    {
+        var combine = new CombineInstance[sourceMeshFilters.Count];
+
+        for (var i = 0; i < sourceMeshFilters.Count; i++)
+        {
+            combine[i].mesh = sourceMeshFilters[i].sharedMesh;
+            combine[i].transform = sourceMeshFilters[i].transform.localToWorldMatrix;
+        }
+
+        var mesh = new Mesh();
+        mesh.CombineMeshes(combine);
+        return mesh;
+    }
+}
